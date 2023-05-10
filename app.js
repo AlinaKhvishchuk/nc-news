bd326053f69eace306f1777b80da308c0b2d2339
Alina Khvishchuk <alina.khvishchuk@gmail.com>
2023-05-10 11:00:43 +0100

    nc-news-3 updated

--- app.js
+++ app.js
@@ -3,16 +3,7 @@ const { getStatus } = require("./controllers/getStatus.controllers");
 const { getTopics } = require("./controllers/controllers.js");
 
 app = express();
-app.get("/api", getStatus);
-app.get("/api/topics", getTopics);
 
-app.get("/api/error", (req, res, next) => {
-  next(new Error("Something went wrong!"));
-});
-app.use((err, req, res, next) => {
-  if (err) {
-    res.status(500).send({ msg: "Server err! We are sorry!" });
-  }
-});
+app.get("/api/topics", getTopics);
 
 module.exports = app;
