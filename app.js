7fed613115da2b227de8c1017b5147a061b4e6a4
Alina Khvishchuk <alina.khvishchuk@gmail.com>
2023-05-11 10:06:39 +0100

    nc-news-4

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const { getStatus } = require("./controllers/getStatus.controllers");
-const { getTopics } = require("./controllers/controllers.js");
+const { getTopics, getArticleById } = require("./controllers/controllers.js");
 
 app = express();
 app.get("/api", getStatus);
 app.get("/api/topics", getTopics);
+app.get("/api/articles/:article_id", getArticleById);
 
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad request" });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ msg: "Server err" });
+});
 module.exports = app;
