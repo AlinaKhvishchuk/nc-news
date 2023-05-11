1075b7dea09e36abc5bb32332fd3691379d08b9d
Alina Khvishchuk <alina.khvishchuk@gmail.com>
2023-05-11 11:32:02 +0100

    nc-news-5

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const { getStatus } = require("./controllers/getStatus.controllers");
-const { getTopics, getArticleById } = require("./controllers/controllers.js");
+const {
+  getTopics,
+  getArticles,
+  getArticleById,
+} = require("./controllers/controllers.js");
 
 app = express();
 app.get("/api", getStatus);
 app.get("/api/topics", getTopics);
+app.get("/api/articles", getArticles);
 app.get("/api/articles/:article_id", getArticleById);
 
 app.use((err, req, res, next) => {
