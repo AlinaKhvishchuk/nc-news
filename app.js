a832fa85219f8ab09548aa6a55f696bdcdb3b35f
Alina Khvishchuk <alina.khvishchuk@gmail.com>
2023-05-30 22:08:24 +0100

    Add cors settings

--- app.js
+++ app.js
@@ -12,12 +12,8 @@ const {
   getUsers,
 } = require("./controllers/controllers.js");
 const app = express();
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+app.use(cors({ origin: "http://localhost:5173" }));
+
 // app.use(cors());
 app.use(express.json());
 
