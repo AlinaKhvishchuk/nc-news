ab4502df6dd45b38e9d1ae6e65194df38cf2687d
Alina Khvishchuk <alina.khvishchuk@gmail.com>
2023-05-30 21:45:06 +0100

    add cors settings

--- app.js
+++ app.js
@@ -11,9 +11,14 @@ const {
   deleteComment,
   getUsers,
 } = require("./controllers/controllers.js");
-
 const app = express();
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+// app.use(cors());
 app.use(express.json());
 
 app.get("/api", getStatus);
